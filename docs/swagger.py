2ac898ec641db2f44cc30127b23ddd1abac08c83
lavi02 <18ds10535@dshs.kr>
2021-12-10 00:48:32 +0900

    lavi02 - push for new reverted-git

---
+++ docs/swagger.py
@@ -0,0 +1,17 @@
+template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Team2_DinosaurGame",
+        "description": "API list for Team2's DinosaurGame.",
+        "contact": {
+            "responsibleOrganization": "https://github.com/2021-HYU-opensource/Team2_DinosaurGame",
+            "responsibleDeveloper": "hoho0259@hanyang.ac.kr",
+            "email": "hoho0259@hanyang.ac.kr",
+            "url": "https://github.com/2021-HYU-opensource/Team2_DinosaurGame"
+        },
+        "version": "1.0.0",
+        "host": "http://ec2-54-180-119-201.ap-northeast-2.compute.amazonaws.com",
+        "basePath": "http://ec2-54-180-119-201.ap-northeast-2.compute.amazonaws.com/user/v1",
+        "schemes": "http"
+    }
+}
